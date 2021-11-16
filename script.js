001a20bb8b07479f0e0fd3a0ccf6e29a2309ace1
dniaa <daniaariesya944@gmail.com>
2021-11-16 11:19:29 +0800

    BMI

---
+++ script.js
@@ -0,0 +1,14 @@
+function BMI() {
+    var w = document.getElementById('w').value;
+    var h = document.getElementById('h').value;
+    var bmi = w / (h * h);
+    document.getElementById("result").innerHTML = "Your BMI is " + bmi.toFixed(2);
+}
+
+function LOOP() {
+    a = 7; c = "";
+    for(b = 1; b < 13; b++) {
+        c += b+ " * " + a+ " = " + (a+b) + "<br>";
+    }
+    loop.innerHTML = c;
+}
